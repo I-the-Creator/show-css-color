9d7d916e046290322a268625a1e4b9232ed87d0e
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-29 14:09:53 +0100

    states and components with props inserted into App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+// Components
+import Square from './Square';
+import Input from'./Input';
+import { useState } from 'react';
 
 function App() {
+  const [colorValue, setColorValue] = useState('');
+  const [hexValue, setHexValue] = useState('');
+  const [isDarkText, setIsDarkText] = useState(true); // state for checking whether color is dark
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square
+        // pass the props
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        // pass the props
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        isDarkText={isDarkText}
+        setIsDarkText={setIsDarkText}
+      />
     </div>
   );
 }
